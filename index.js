d39725cf9373e5226205c50cbe7dde1109882f3a
Jbsy007 <justintom00726@gmail.com>
2026-06-26 17:17:57 +0900

    コミット5

--- index.js
+++ index.js
@@ -10,65 +10,39 @@ let tasks = [
 /**
  * [学生1]
  * 引数で与えられるタイトルで「やること」が作成されるように修正してください。
- * 
- * 注) 登録ボタンを押したときに、この関数が呼ばれる処理は既に書かれています。
- * 
  */
 function addTask(title) {
     const task = { isDone: false }
 
-    // やることのタイトルを設定(このままだと、すべてのタイトルがHello Worldになってしまう！)
-    task.title = "Hello World"
+    // 入力されたタイトルを設定
+    task.title = title;
 
-    // 以降は無視して良い
     tasks.push(task);
     drawTask(task, tasks.length - 1);
 }
 
 /**
  * [学生2]
- * 「チェックボックス」をクリックしたときに、
- * 引数で与えられる「やること」の達成状態(isDone)を、
- * 現在の状態と反対になるように更新してください。
- * 
- * 注) クリックしたときに、この関数が呼ばれる処理は既に書かれています。
- * 
- * 例:
- * before   |   after
- * --------------------
- * true     |   false
- * false    |   true
+ * チェックボックスをクリックしたとき
  */
 function onUpdateIsDone(task) {
     console.log("チェックボックスがクリックされました。", task);
 
-    // 達成状態を更新(このままだと、現在の達成状態で更新されてしまう！)
-    const currentValue = task.isDone;
-    task.isDone = currentValue;
+    // true⇔falseを切り替える
+    task.isDone = !task.isDone;
 
     return task;
 }
 
 /**
  * [学生3]
- * 「タスクのタイトル」をクリックしたときに、
- * 引数で与えられる「やること」の達成状態(isDone)を、
- * 現在の状態と反対になるように更新してください。
- * 
- * 注) クリックしたときに、この関数が呼ばれる処理は既に書かれています。
- * 
- * 例:
- * before   |   after
- * --------------------
- * true     |   false
- * false    |   true
+ * タイトルをクリックしたとき
  */
 function onTaskTitleClicked(task) {
     console.log("タイトルがクリックされました。", task);
 
-    // 達成状態を更新(このままだと、現在の達成状態で更新されてしまう！)
-    const currentValue = task.isDone;
-    task.isDone = currentValue;
+    // true⇔falseを切り替える
+    task.isDone = !task.isDone;
 
     return task;
 }
